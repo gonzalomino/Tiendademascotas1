f39a9b91351cbe62b23a44b0a32eaeab93615e9e
“Nicolas <dpmelectricidad23@gmail.com>
2024-05-06 17:19:34 -0300

    Nico 06 05 24

--- STATIC/js/turnos.js
+++ STATIC/js/turnos.js
@@ -1,11 +1,14 @@
 //validar datos de formulario Turnos//
 function validardatos()
  {
-let nombre = document.getElementsByName ("nombre")
-let apellido = document.getElementsByName ("apellido")
-let edad = Number.getElementsByName ("edad")
-let email = email.getElementsByName("email")
-let nombredemascota = document.getElementsByName ("nombredemascota")
+let nombre = document.getElementsByName ("nombre").value.trim()
+let apellido = document.getElementsByName ("apellido").value.trim()
+let edad = Number.getElementsByName ("edad").value.trim()
+let email = email.getElementsByName("email").value.trim
+let nombredemascota = document.getElementsByName ("nombredemascota").value.trim
+let error= document.getElementById ("campos");
 if (nombre=== "" || nombredemascota === ""){
-    error.texcontent = "Por favor completar datos para poder enviar el formulario"}
- }
+    error.texcontent = "Por favor completar datos para pode r enviar el formulario")
+    error.style.color ="red"
+    return false
+}
