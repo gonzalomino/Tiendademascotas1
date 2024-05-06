7ca3748d5409a29c36810781c453842d54470e05
“Nicolas <dpmelectricidad23@gmail.com>
2024-05-05 21:35:10 -0300

    Nico 02 05 24 c

--- STATIC/js/turnos.js
+++ STATIC/js/turnos.js
@@ -1,5 +1,5 @@
 //validar datos de formulario Turnos//
-function validardatos ()
+function validardatos()
  {
 let nombre = document.getElementsByName ("nombre")
 let apellido = document.getElementsByName ("apellido")
@@ -7,5 +7,5 @@ let edad = Number.getElementsByName ("edad")
 let email = email.getElementsByName("email")
 let nombredemascota = document.getElementsByName ("nombredemascota")
 if (nombre=== "" || nombredemascota === ""){
-    error.texcontent "Por favor completar datos para poder enviar el formulario")}
+    error.texcontent = "Por favor completar datos para poder enviar el formulario"}
  }
