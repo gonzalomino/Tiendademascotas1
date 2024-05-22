bc46c0efa950c945568a26b88459cce530fc7200
Gonzalo Miño <gonzalomi7@hotmail.com>
2024-05-22 11:47:50 -0300

    cform

--- STATIC/js/turnos.js
+++ STATIC/js/turnos.js
@@ -1,10 +1,10 @@
 //validar datos de formulario Turnos//
-function validardatos()
+function validarDatos()
  {
 let nombre = document.getElementsByName ("nombre").value.trim()
 let apellido = document.getElementsByName ("apellido").value.trim()
-let edad = Number.getElementsByName ("edad").value.trim()
-let email = email.getElementsByName("email").value.trim
+let edad = document.getElementsByName ("edad").value.trim()
+let email = document.getElementsByName("email").value.trim
 let nombredemascota = document.getElementsByName ("nombredemascota").value.trim
 let error= document.getElementById ("campos");
 if (nombre=== "" || nombredemascota === ""){
